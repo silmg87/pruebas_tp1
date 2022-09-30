74c76d19f493b521d21b588862d301204470d4bf
Silvina <silvinagattas@gmail.com>
2022-09-30 19:14:33 -0300

    actualizacion
    con array de iconos!

--- script/appClima.js
+++ script/appClima.js
@@ -1,6 +1,122 @@
+'use strict';
+
+// ARRAY DE ICONOS
+const iconos = [
+    {
+        id: '01d',
+        descripcion: 'Soleado',
+        imagen: '../imagenes/soleado.png',
+        alt: 'Día Soleado',
+    },
+    {
+        id: '01n',
+        descripcion: 'Cielo despejado',
+        imagen: '../imagenes/noche-despejada.png',
+        alt: 'Noche Despejada',
+    },
+    {
+        id: '02d',
+        descripcion: 'Parcialmente nublado con sol',
+        imagen: '../imagenes/dia-nublado.png',
+        alt: 'Día Nublado',
+    },
+    {
+        id: '02n',
+        descripcion: 'Pacialmente nublado',
+        imagen: '../imagenes/noche-nublada.png',
+        alt: 'Noche Nublada',
+    },
+    {
+        id: '03d',
+        descripcion: 'Mayormente nublado',
+        imagen: '../imagenes/nubes.png',
+        alt: 'Día Nublado',
+    },
+    {
+        id: '03n',
+        descripcion: 'Mayormente nublado',
+        imagen: '../imagenes/nubes.png',
+        alt: 'Noche Nublada',
+    },
+    {
+        id: '04d',
+        descripcion: 'Mayormente nublado',
+        imagen: '../imagenes/nubes.png',
+        alt: 'Día Nublado',
+    },
+    {
+        id: '04n',
+        descripcion: 'Mayormente nublado',
+        imagen: '../imagenes/nubes.png',
+        alt: 'Noche Nublada',
+    },  
+    {
+        id: '09d',
+        descripcion: 'Cielo cubierto con lloviznas',
+        imagen: '../imagenes/llovizna.png',
+        alt: 'Día con lloviznas',
+    },
+    {
+        id: '09n',
+        descripcion: 'Cielo cubierto con lloviznas',
+        imagen: '../imagenes/llovizna.png',
+        alt: 'Noche con lloviznas',
+    },
+    {
+        id: '10d',
+        descripcion: 'Parcialmente cubierto con lluvias',
+        imagen: '../imagenes/dia-lluvioso.png',
+        alt: 'Día con lluvias',
+    },
+    {
+        id: '10n',
+        descripcion: 'Parcialmente cubierto con lluvias',
+        imagen: '../imagenes/noche-lluviosa.png',
+        alt: 'Noche con luvias',
+    },
+    {
+        id: '11d',
+        descripcion: 'Tormentas eléctricas',
+        imagen: '../imagenes/tormenta.png',
+        alt: 'Día con tormenta',
+    },
+    {
+        id: '11n',
+        descripcion: 'Tormentas eléctricas',
+        imagen: '../imagenes/tormenta.png',
+        alt: 'Noche con tormenta',
+    },
+    {
+        id: '13d',
+        descripcion: 'Nevadas',
+        imagen: '../imagenes/nieve.png',
+        alt: 'Día con nevadas',
+    },
+    {
+        id: '13n',
+        descripcion: 'Nevadas',
+        imagen: '../imagenes/nieve.png',
+        alt: 'Noche con nevadas',
+    },
+    {
+        id: '50d',
+        descripcion: 'Neblinas',
+        imagen: '../imagenes/nieblas.png',
+        alt: 'Día con nieblas',
+    },
+    {
+        id: '50n',
+        descripcion: 'Neblinas',
+        imagen: '../imagenes/nieblas.png',
+        alt: 'Noche con nieblas',
+    },
+];
+
+// DECLARACION DE VARIABLES
 const APIKEYCLIMA = '9dcc1818ed8b1b9cf09902249070fb83';
 const btn = document.getElementById('buscar');
 const inputCiudad = document.getElementById('inputCiudad');
+
 const resultadoTemp = document.getElementById('temperaturaActual');
 const resultadoZona = document.getElementById('zona');
 const resultadoIcono = document.getElementById('icono');
@@ -12,48 +128,60 @@ let icono = '';
 let info = '';
 let divMapa = '';
 
-// function mostrarBusquedas() {
-//     if(localStorage.busqueda){
-//         recuperar_localStorage = JSON.parse(localStorage.getItem("busqueda"));
-//         let guardarDatos = '';
-//     for (let clave in recuperar_localStorage) {
-//         guardarDatos += `<div>
-//                             <h2>Vistos recientemente</h2>
-//                             <p class="datos">
-//                             <span>${recuperar_localStorage[clave].ciudad}</span>
-//                             <span>${recuperar_localStorage[clave].temp}</span>
-//                             <span>${recuperar_localStorage[clave].info.descripcion}</span>
-//                             <span>${recuperar_localStorage[clave].info.max}</span>
-//                             <span>${recuperar_localStorage[clave].info.min}</span>
-//                             <span>${recuperar_localStorage[clave].info.hum}</span>
-//                             <span>${recuperar_localStorage[clave].info.st}</span>
-//                             <span>${recuperar_localStorage[clave].info.presAt}</span>
-//                             <span>${recuperar_localStorage[clave].info.velViento}</span>
-//                             </p>
-//                         </div>`
-//     }
-//     document.querySelector("#mostrarAnterior").innerHTML = guardarDatos;
-//     }
-// }
+// FUNCION PARA MOSTRAR DATOS DEL LOCALSTORAGE
+function mostrarBusquedas() {
+    if(localStorage.busqueda){
+        recuperar_localStorage = JSON.parse(localStorage.getItem("busqueda"));
+        let guardarDatos = '';
+    for (let clave in recuperar_localStorage) {
+        guardarDatos += `<div>
+                            <h2>Vistos recientemente</h2>
+                            <p class="datos">
+                            <span>${recuperar_localStorage[clave].ciudad}</span>
+                            <span>${recuperar_localStorage[clave].temp}</span>
+                            <span>${recuperar_localStorage[clave].info.descripcion}</span>
+                            <span>${recuperar_localStorage[clave].info.max}</span>
+                            <span>${recuperar_localStorage[clave].info.min}</span>
+                            <span>${recuperar_localStorage[clave].info.hum}</span>
+                            <span>${recuperar_localStorage[clave].info.st}</span>
+                            <span>${recuperar_localStorage[clave].info.presAt}</span>
+                            <span>${recuperar_localStorage[clave].info.velViento}</span>
+                            </p>
+                        </div>`
+    }
 
+    document.querySelector("#mostrarAnterior").innerHTML = guardarDatos;
+
+    }
+}
+
+// EVENTO CLICK "BUSCAR"
 btn.addEventListener('click', event => {
     event.preventDefault();
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputCiudad.value}&appid=${APIKEYCLIMA}&units=metric&lang=es`)
-    .then(respuesta=>{
-        console.log(`Respuesta: ${respuesta}`, respuesta);
-        return respuesta.json();
+    .then(response=>{
+        console.log(`Respuesta: ${response}`, response);
+        return response.json();
     })
 
     .then(json=>{
         console.log(json)
 
+        let divZona = document.createElement('div');
+
         divZona = `<p>${json.name}</p>`;
         resultadoZona.innerHTML = divZona;
         divZona = '';
 
-        icono = json.weather[0].icon;
+        for (let i of iconos) {
+            let icon = json.weather[0].icon;
+            if (icon === i.id) {
+                icono = `<img src="${i.imagen}" alt="${i.alt}"/>`;
+                resultadoIcono.innerHTML = icono;
+            } 
+            console.log(icon);
+        }
         
-        resultadoIcono.innerHTML = icono;
         icono = '';
 
         tempActual = `<p>${Math.round(json.main.temp)}°</p>`;
@@ -81,7 +209,7 @@ btn.addEventListener('click', event => {
                 array_clima = JSON.parse(localStorage.busqueda);
             }
                 
-            datosClima = {
+            let datosClima = {
                 ciudad: json.name,
                 temp: Math.round(json.main.temp),
                 info: {
@@ -106,7 +234,5 @@ btn.addEventListener('click', event => {
 
 });
 
-// mostrarBusquedas();
-// document.body.onload = mostrarBusquedas();
 
 
